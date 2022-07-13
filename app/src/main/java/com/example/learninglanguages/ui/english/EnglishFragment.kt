a7983a5f569e430b72fa6a7ae625d99e65733b30
Paul <paulik0182@gmail.com>
2022-07-13 16:47:10 +0300

    Инициализация объектов в разметке

--- app/src/main/java/com/example/learninglanguages/ui/english/EnglishFragment.kt
+++ app/src/main/java/com/example/learninglanguages/ui/english/EnglishFragment.kt
@@ -4,12 +4,19 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.learninglanguages.App
 import com.example.learninglanguages.R
 import com.example.learninglanguages.domain.english.EnglishRepo
 
 class EnglishFragment : Fragment() {
+
+    private lateinit var taskEnglishTv: TextView
+    private lateinit var answerEnglishTv1: TextView
+    private lateinit var answerEnglishTv2: TextView
+    private lateinit var answerEnglishTv3: TextView
+    private lateinit var answerEnglishTv4: TextView
 
     private val add: App by lazy { requireActivity().application as App }
 
@@ -34,6 +41,11 @@ class EnglishFragment : Fragment() {
 
     private fun initViews(view: View) {
 
+        taskEnglishTv = view.findViewById(R.id.task_english_text_view)
+        answerEnglishTv1 = view.findViewById(R.id.answer1_english_text_view)
+        answerEnglishTv2 = view.findViewById(R.id.answer2_english_text_view)
+        answerEnglishTv3 = view.findViewById(R.id.answer3_english_text_view)
+        answerEnglishTv4 = view.findViewById(R.id.answer4_english_text_view)
 
     }
 }
