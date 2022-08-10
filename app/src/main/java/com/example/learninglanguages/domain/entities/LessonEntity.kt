72502b596e74fcbd155f2d318bc55b5c0e6f7f2b
Paul <paulik0182@gmail.com>
2022-08-10 14:38:42 +0300

    Correction Entity (для корректного чтения json файла с отличающимися в названии полями)

--- app/src/main/java/com/example/learninglanguages/domain/entities/LessonEntity.kt
+++ app/src/main/java/com/example/learninglanguages/domain/entities/LessonEntity.kt
@@ -1,16 +1,21 @@
 package com.example.learninglanguages.domain.entities
 
 import android.os.Parcelable
+import com.google.firebase.database.PropertyName
 import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class LessonEntity(
-    val id: Long,
-    val name: String,
+    val id: Long = 0,
+    val name: String = "",
     @SerializedName("image_url")
-    val imageUrl: String,
+    @PropertyName("image_url")
+    @get: PropertyName("image_url")
+    val imageUrl: String = "",
     @SerializedName("victory_image_url")
+    @PropertyName("victory_image_url")
+    @get: PropertyName("victory_image_url")
     val victoryImageUrl: String? = null,
-    val tasks: List<TaskEntity>
+    val tasks: List<TaskEntity>? = null
 ) : Parcelable
--- app/src/main/java/com/example/learninglanguages/domain/entities/TaskEntity.kt
+++ app/src/main/java/com/example/learninglanguages/domain/entities/TaskEntity.kt
@@ -1,18 +1,25 @@
 package com.example.learninglanguages.domain.entities
 
 import android.os.Parcelable
+import com.google.firebase.database.PropertyName
 import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class TaskEntity(
-    val id: Long,
-    val task: String,
+    val id: Long = 0,
+    val task: String = "",
     @SerializedName("task_image_url")
+    @PropertyName("task_image_url")
+    @get: PropertyName("task_image_url")
     val taskImageUrl: String? = null,
     @SerializedName("variants_answer")
-    val variantsAnswer: List<String>,
+    @PropertyName("variants_answer")
+    @get: PropertyName("variants_answer")
+    val variantsAnswer: List<String>? = null,
     @SerializedName("right_answer")
-    val rightAnswer: String,
-    val level: Int
+    @PropertyName("right_answer")
+    @get: PropertyName("right_answer")
+    val rightAnswer: String = "",
+    val level: Int = 0
 ) : Parcelable
