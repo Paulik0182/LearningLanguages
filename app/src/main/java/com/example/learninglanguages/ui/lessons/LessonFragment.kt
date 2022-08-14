aa4d17ceeb8b226346f5476c13220ddd878802bf
Paul <paulik0182@gmail.com>
2022-08-14 15:16:54 +0300

    LessonFragment -> show all

---
+++ app/src/main/java/com/example/learninglanguages/ui/lessons/LessonFragment.kt
@@ -0,0 +1,79 @@
+package com.example.learninglanguages.ui.lessons
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.learninglanguages.App
+import com.example.learninglanguages.R
+import com.example.learninglanguages.domain.entities.LessonEntity
+import com.example.learninglanguages.domain.repos.CoursesRepo
+
+class LessonFragment : Fragment() {
+
+    private val app: App by lazy { requireActivity().application as App }
+    private lateinit var adapter: CoursesAdapter
+
+
+    private lateinit var lessonsRecyclerView: RecyclerView
+    private val coursesRepo: CoursesRepo by lazy {
+        app.coursesRepo
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_lesson, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initViews()
+        initDate()
+
+    }
+
+    private fun initDate() {
+        // кулбек
+        coursesRepo.getCourses {
+            adapter.setData(it)// пополнение адаптера данными
+        }
+    }
+
+    private fun initViews() {
+        //можжно сделать такую комбинацию для инициализации переменной, чтобы в каждой строке не
+        // добавлять view и не передовать view дополнительно
+        //это значит, если view существует то выполнить следующее (apply это где аргумент
+        // передается this. это такая комбинация где вместо this подставляется то что слево от apply)
+        view?.apply {
+            lessonsRecyclerView = findViewById(R.id.lessons_recycler_view)
+        }
+
+        //это два параметра которые принимаем на вход. Это слушатель и данные
+        lessonsRecyclerView.layoutManager = LinearLayoutManager(context)
+
+        //кэшируем адаптер чтобы его потом вызвать
+        adapter = CoursesAdapter() {
+            getController().openLesson(it)
+        }
+        lessonsRecyclerView.adapter = adapter
+    }
+
+    private fun getController(): Controller = activity as Controller
+
+    interface Controller {
+        fun openLesson(lessonEntity: LessonEntity)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        getController()  //Вариант 2. агресивный способ проверки наличия контроллера. Если нет контроллера, приложение свалтится на присоединение к фрагмента к активити
+    }
+}
