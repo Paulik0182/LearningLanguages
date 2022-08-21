b9db53d5ff3c73c07c88fde4b1a3a35ae8ad827f
Paul <paulik0182@gmail.com>
2022-08-21 20:45:33 +0300

    Проблемы MVP

--- app/src/main/java/com/example/learninglanguages/ui/courses/CoursesPresenter.kt
+++ app/src/main/java/com/example/learninglanguages/ui/courses/CoursesPresenter.kt
@@ -4,29 +4,48 @@ import com.example.learninglanguages.domain.entities.CourseEntity
 import com.example.learninglanguages.domain.entities.LessonEntity
 import com.example.learninglanguages.domain.repos.CoursesRepo
 
+/**
+ * Проблемы MVP
+ * 1.Знание презентора о view
+ * 2.Хранить и востанавливать состояние (все значеия) в аттач
+ * 3.Возвращать все вызовы на главвный поток (нарушать "незнание" проезентора
+ * 4*. Не поддерживает гугл и платформой (пишем сами или ищем библиотеки)
+ */
+
 class CoursesPresenter(
     private val coursesRepo: CoursesRepo//Экзепляр класса (достали repo) (ЭТО АРГУМЕНТ КОНСТРУКТОРА. private val - это автоматически делает членом класса)
 ) : CoursesContract.Presenter {
 
     // для то чтобы воспользоватся attach view необходимо ее запомнить
     private var view: CoursesContract.View? = null
-
-    private var courses: MutableList<CourseEntity>? = null//Кэшируем курсы
+    private var inProgress: Boolean = false //кэшируем Progress
+        private set(value) {
+            field = value
+            view?.showProgress(value)
+        }
+    private var courses: MutableList<CourseEntity> = mutableListOf()
+        //Кэшируем курсы
+        private set(value) {
+            field = value
+            view?.setCourses(value)
+        }
 
     override fun attach(view: CoursesContract.View) {
         this.view = view
 
-        view.showProgress(false)
-        val localCourses = courses//еще раз закэшировали курсы
-        if (localCourses == null) {// проверка если courses равно null, то мы качаем курсы, если нет то подставляем уже закэшированные курсы
-            view.showProgress(true)
+//        view.showProgress(inProgress)
+        inProgress = inProgress
+//        val localCourses = courses//еще раз закэшировали курсы
+        if (courses.isEmpty()) {// проверка если courses равно null, то мы качаем курсы, если нет то подставляем уже закэшированные курсы
+            inProgress = true
             coursesRepo.getCourses {
-                view.showProgress(false)
-                view.setCourses(it)// пополнение данных
+                inProgress = false
+//                view.setCourses(it)// пополнение данных
                 courses = it
             }
         } else {
-            view.setCourses(localCourses)// пополнение данных
+            courses = courses
+//            view.setCourses(localCourses)// пополнение данных
         }
     }
 
