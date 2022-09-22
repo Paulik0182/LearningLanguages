ef1b82d069ae6ea198f6e5a54d543d51e0353008
Paul <paulik0182@gmail.com>
2022-09-22 16:28:10 +0300

    Напрямую во LessonViewHolder завели FavoriteLessonsRepo (с помощью Koin)

--- app/src/main/java/com/example/learninglanguages/ui/lessons/LessonViewHolder.kt
+++ app/src/main/java/com/example/learninglanguages/ui/lessons/LessonViewHolder.kt
@@ -9,14 +9,15 @@ import com.example.learninglanguages.R
 import com.example.learninglanguages.domain.entities.FavoriteLessonEntity
 import com.example.learninglanguages.domain.repos.FavoriteLessonsRepo
 import com.squareup.picasso.Picasso
+import org.koin.java.KoinJavaComponent.inject
 
 class LessonViewHolder(
     itemView: View,
-    private var likesStorage: FavoriteLessonsRepo,
     listener: (Long, FavoriteLessonEntity) -> Unit
 ) :
     RecyclerView.ViewHolder(itemView) {
 
+    private val favoriteRepo: FavoriteLessonsRepo by inject(FavoriteLessonsRepo::class.java)
     private val titleTextView = itemView.findViewById<TextView>(R.id.title_text_view)
     private val coverImageView = itemView.findViewById<ImageView>(R.id.cover_image_view)
     private var favoriteImageView = itemView.findViewById<ImageView>(R.id.favorite_image_view)
@@ -35,7 +36,7 @@ class LessonViewHolder(
                 .into(coverImageView)
 //        coverImageView.scaleType = ImageView.ScaleType.FIT_XY// растягиваем картинку на весь элемент
         }
-        favoriteImageView.isVisible = likesStorage.isFavorite(courseId, lessonEntity.id)
+        favoriteImageView.isVisible = favoriteRepo.isFavorite(courseId, lessonEntity.id)
     }
 
     init {
--- app/src/main/java/com/example/learninglanguages/ui/lessons/LessonsAdapter.kt
+++ app/src/main/java/com/example/learninglanguages/ui/lessons/LessonsAdapter.kt
@@ -35,7 +35,7 @@ class LessonsAdapter(
                 },
                 parent,
                 false
-            ), likesRepo,
+            ),
             listener
         )
     }
